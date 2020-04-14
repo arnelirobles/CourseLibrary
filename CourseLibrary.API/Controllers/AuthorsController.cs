d2ce15951ba3ecca5ca908a3ed8b737aa1a105ab
Arnel Robles <arobles@lemontechsolutions.com>
2020-04-14 22:46:50 +0800

    Added the ff GetAuthor GetAuthors

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -8,18 +8,29 @@ using System.Threading.Tasks;
 namespace CourseLibrary.API.Controllers
 {
     [ApiController]
+    [Route("api/authors")]
     public class AuthorsController : ControllerBase
     {
         private readonly ICourseLibraryRepository _courseLibraryRepository;
 
         public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
         {
-            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(CourseLibraryRepository));
+            _courseLibraryRepository = courseLibraryRepository ?? 
+                throw new ArgumentNullException(nameof(CourseLibraryRepository));
         }
+
+        [HttpGet("")] 
         public IActionResult GetAuthors()
         {
             var authors = _courseLibraryRepository.GetAuthors();
             return new JsonResult(authors);
+        }
+
+        [HttpGet ("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var author = _courseLibraryRepository.GetAuthor(authorId);
+            return new JsonResult(author);
         }
     }
 }
