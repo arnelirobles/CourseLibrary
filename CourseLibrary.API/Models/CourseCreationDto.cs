7a381b18502149c5bc00c4c24cff0426095cc4ba
Arnel Robles <arobles@lemontechsolutions.com>
2020-04-15 18:19:37 +0800

    Added custom validation attributes custom error messages

--- CourseLibrary.API/Models/CourseCreationDto.cs
+++ CourseLibrary.API/Models/CourseCreationDto.cs
@@ -1,24 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseCreationDto : IValidatableObject
+    [CourseTitleMustBeDifferentFromDescription]
+    public class CourseCreationDto //: IValidatableObject
     {
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage = "Kindly fill out title.")]
+        [MaxLength(100, ErrorMessage = "Title must be not be more than 100 chars.")]
         public string Title { get; set; }
 
-        [MaxLength(1500)]
+        [MaxLength(1500, ErrorMessage = "Description must be not be more than 1500 chars.")]
         public string Description { get; set; }
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (Title == Description)
-            {
-                yield return new ValidationResult("The Description must not be the same with Title.",
-                    new[] { nameof(CourseCreationDto) });
-            }
-        }
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult("The Description must not be the same with Title.",
+        //            new[] { nameof(CourseCreationDto) });
+        //    }
+        //}
     }
 }
---
+++ CourseLibrary.API/ValidationAttributes/CourseTitleMustBeDifferentFromDescriptionAttribute.cs
@@ -0,0 +1,23 @@
+﻿using CourseLibrary.API.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourseLibrary.API.ValidationAttributes
+{
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = false)]
+    public sealed class CourseTitleMustBeDifferentFromDescriptionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var course = (CourseCreationDto)validationContext.ObjectInstance;
+
+            if (course.Title == course.Description)
+            {
+                return new ValidationResult("The Description must not be the same with Title.",
+                    new[] { nameof(CourseCreationDto) });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
