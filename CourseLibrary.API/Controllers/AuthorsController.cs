c244e0faef7e404a28fed27d0499207134cb0d77
Arnel Robles <arobles@lemontechsolutions.com>
2020-04-15 09:18:26 +0800

    Added the ff Automapper CoursesController Custom Exception Handler

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -1,6 +1,9 @@
-﻿using CourseLibrary.API.Services;
+﻿using AutoMapper;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 
 namespace CourseLibrary.API.Controllers
 {
@@ -9,29 +12,33 @@ namespace CourseLibrary.API.Controllers
     public class AuthorsController : ControllerBase
     {
         private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
 
-        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
         {
             _courseLibraryRepository = courseLibraryRepository ??
-                throw new ArgumentNullException(nameof(CourseLibraryRepository));
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
         }
 
         [HttpGet("")]
-        public IActionResult GetAuthors()
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthors()
         {
-            var authors = _courseLibraryRepository.GetAuthors();
+            var authors = _mapper.Map<IEnumerable<AuthorDto>>(_courseLibraryRepository.GetAuthors());
+
             return Ok(authors);
         }
 
         [HttpGet("{authorId}")]
-        public IActionResult GetAuthor(Guid authorId)
+        public ActionResult<AuthorDto> GetAuthor(Guid authorId)
         {
             //if (!_courseLibraryRepository.AuthorExists(authorId))
             //{
             //    return NotFound();
             //}
 
-            var author = _courseLibraryRepository.GetAuthor(authorId);
+            var author = _mapper.Map<AuthorDto>(_courseLibraryRepository.GetAuthor(authorId));
 
             if (author == null)
             {
---
+++ CourseLibrary.API/Profiles/AuthorsProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Entities;
+using CourseLibrary.API.Helpers;
+using CourseLibrary.API.Models;
+
+namespace CourseLibrary.API.Profiles
+{
+    public class AuthorsProfile : Profile
+    {
+        public AuthorsProfile()
+        {
+            CreateMap<Author, AuthorDto>()
+                .ForMember(
+                    d => d.Name,
+                    o => o.MapFrom(s => $"{s.FirstName} {s.LastName}"))
+                .ForMember(
+                    d => d.Age,
+                    o => o.MapFrom(s => s.DateOfBirth.GetCurrentAge()))
+                ;
+        }
+    }
+}
